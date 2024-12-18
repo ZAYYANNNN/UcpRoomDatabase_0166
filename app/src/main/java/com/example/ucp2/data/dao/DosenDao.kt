35ba3b70fdaf7233060c6a99392944fab13036b4
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-18 10:31:26 +0700

    membuat query untuk crud pada tabel dosen dan matkul

--- app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
@@ -1,4 +1,17 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
 interface DosenDao {
+    @Insert
+    suspend fun insertDosen(dosen: Dosen)
+
+    @Query ("Select * FROM dosen ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Dosen>>
+
+    @Query("SELECT * FROM dosen WHERE Nidn = :Nidn")
+    fun getMahasiswa (Nidn: String): Flow<Dosen>
 }
--- app/src/main/java/com/example/ucp2/data/dao/MatkulDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/MatkulDao.kt
@@ -1,4 +1,25 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.Matkul
+import kotlinx.coroutines.flow.Flow
+
 interface MatkulDao {
+    @Insert
+    suspend fun insertMatkul(matkul: Matkul)
+
+    @Query ("Select * FROM matkul ORDER BY namaMK ASC")
+    fun getAllMatkul() : Flow<List<Matkul>>
+
+    @Query("SELECT * FROM matkul WHERE kdMK = :kdMK")
+    fun getMatkul (kdMK: String): Flow<Matkul>
+
+    @Delete
+    suspend fun deleteMatkul(matkul: Matkul)
+
+    @Update
+    suspend fun updateMatkul(matkul: Matkul)
 }
