671d224c748c9d9b44394de98034f47544f1f8b4
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-12-18 14:15:36 +0700

    mengganti dosendatabase dan matkuldatabase menjadi KrsDatabase.kt

--- app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
@@ -4,27 +4,31 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DosenDao
 import com.example.ucp2.data.dao.MatkulDao
+import com.example.ucp2.data.entity.Dosen
 import com.example.ucp2.data.entity.Matkul
 
-@Database(entities = [Matkul::class], version = 1, exportSchema = false)
-abstract class matkulDatabase : RoomDatabase() {
+@Database(entities = [Dosen::class],[Matkul::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase() {
+    //Mendefinisikan fungsi untuk mengakses data Mahasiswa
+
+    abstract fun dosenDao(): DosenDao
     abstract fun matkulDao(): MatkulDao
 
     companion object{
         @Volatile // Memastikan bahwa nilai variabel instance selalu sama di
-        private var Instance: matkulDatabase? = null
+        private var Instance: KrsDatabase? = null
 
-        fun getDatabase(context: Context): matkulDatabase {
+        fun getDatabase(context: Context):KrsDatabase {
             return (Instance ?: synchronized(this) {
                 Room.databaseBuilder(
                     context.applicationContext,
-                    matkulDatabase::class.java, //Class Database
-                    "matkulDatabase" //Nama Database
+                    KrsDatabase::class.java, //Class Database
+                    "KrsDatabase" //Nama Database
                 )
                     .build().also { Instance = it }
             })
-
         }
     }
 }
--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,8 +1,7 @@
 package com.example.ucp2.dependenciesinjection
 
 import android.content.Context
-import com.example.ucp2.data.database.dosenDatabase
-import com.example.ucp2.data.database.matkulDatabase
+import com.example.ucp2.data.database.KrsDatabase
 import com.example.ucp2.repository.LocalRepoDsn
 import com.example.ucp2.repository.LocalRepoMk
 import com.example.ucp2.repository.RepoMk
@@ -15,10 +14,10 @@ interface InterfaceContainerApp {
 
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
     override val repositoryDsn: RepositoryDsn by lazy {
-        LocalRepoDsn(dosenDatabase.getDatabase(context).dosenDao())
+        LocalRepoDsn(KrsDatabase.getDatabase(context).dosenDao())
     }
     override val repoMk: RepoMk by lazy {
-        LocalRepoMk(matkulDatabase.getDatabase(context).matkulDao())
+        LocalRepoMk(KrsDatabase.getDatabase(context).matkulDao())
     }
 
 }
